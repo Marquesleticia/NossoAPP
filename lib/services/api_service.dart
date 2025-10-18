05d6059f7844b5cc0a405f20ecad1d2b45f21e01
Leh Marques <leticiaespindolamarques@outlook.com>
2025-10-17 21:10:24 -0300

    http: ^1.1.0

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'https://your-mockapi-id.mockapi.io';  // Substitua pela sua URL
+  static const String baseUrl = 'https://your-mockapi-id.mockapi.io';  // Substitua pela sua URL <----------
 
   static Future<bool> login(String login, String senha) async {
     final response = await http.get(Uri.parse('$baseUrl/login?login=$login&senha=$senha'));
@@ -48,8 +48,3 @@ class ApiService {
   }
 }
 
-// static Future<void> deletePessoa(String id) async {
-//    final response = await http.delete(Uri.parse('$baseUrl/pessoa/$id'));
- //   if (response.statusCode != 200) throw Exception('Erro');
-//  }
-//}
